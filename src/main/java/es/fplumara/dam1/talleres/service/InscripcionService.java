5d78be83dc898bc0f626b8a6388ff778cd1f46e2
Andrés Jaime Eduardo Díaz Mercado <777@alumnos.fpsumma.es>
2026-02-04 12:07:03 +0100

    inscripcionservice

---
+++ src/main/java/es/fplumara/dam1/talleres/service/InscripcionService.java
@@ -0,0 +1,23 @@
+package es.fplumara.dam1.talleres.service;
+
+import es.fplumara.dam1.talleres.model.Inscripcion;
+import es.fplumara.dam1.talleres.model.Rol;
+
+import java.util.List;
+
+public interface InscripcionService {
+    List<Inscripcion> listarInscripcionesDeUsuario(Long usuarioId);
+
+    List<Inscripcion> verMiembrosAgrupadosPorTaller(Long tallerid);
+
+    List<Inscripcion> verResponsables(Long tallerid);
+
+    Inscripcion inscribirUsuario(Long tallerid, Long usuarioid, Rol rol);
+
+    Inscripcion cambiarRol(Long tallerid, Long usuarioid);
+
+    Inscripcion expulsarUsuario(Long tallerid, Long usuarioid);
+
+    List<Inscripcion> listarInscripcionesDeTaller(Long tallerid);
+}
+
---
+++ src/main/java/es/fplumara/dam1/talleres/service/impl/InscripcionServiceImpl.java
@@ -0,0 +1,90 @@
+package es.fplumara.dam1.talleres.service.impl;
+
+import es.fplumara.dam1.talleres.exception.NotFoundException;
+import es.fplumara.dam1.talleres.model.*;
+import es.fplumara.dam1.talleres.repository.InscripcionRepository;
+import es.fplumara.dam1.talleres.repository.TallerRepository;
+import es.fplumara.dam1.talleres.repository.UserRepository;
+import es.fplumara.dam1.talleres.service.InscripcionService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InscripcionServiceImpl implements InscripcionService {
+    private InscripcionRepository InscripcionRepository;
+
+    private UserRepository UseRepository;
+
+    private TallerRepository TallerRepository;
+
+    public InscripcionServiceImpl(InscripcionRepository inscripcionRepository, TallerRepository tallerRepository, UserRepository useRepository) {
+        this.InscripcionRepository = inscripcionRepository;
+        this.TallerRepository = tallerRepository;
+        this.UseRepository = useRepository;
+    }
+
+    @Override
+    public List<Inscripcion> listarInscripcionesDeTaller(Long tallerid) {
+
+        if (TallerRepository.findById(tallerid) == null){
+            throw new NotFoundException("El taller no existe");
+        }
+
+        return InscripcionRepository.findByTallerId(tallerid);
+    }
+
+    @Override
+    public List<Inscripcion> listarInscripcionesDeUsuario(Long usuarioId) {
+        if (UseRepository.findById(usuarioId) == null){
+            throw new NotFoundException("El usuario no existe");
+        }
+        return InscripcionRepository.findbyUsuarioId(usuarioId);
+    }
+
+    @Override
+    public List<Inscripcion> verMiembrosAgrupadosPorTaller(Long tallerid) {
+        if (TallerRepository.findById(tallerid) == null){
+            throw new NotFoundException("Taller no existe");
+        }
+            List<Usuario> alumno = new ArrayList<>();
+            List<Usuario> profesor = new ArrayList<>();
+            List<Usuario> invitado = new ArrayList<>();
+         List<Inscripcion> inscripciones = InscripcionRepository.findByTallerId(tallerid);
+
+        for (Inscripcion inscripcion : inscripciones){
+            Usuario usuario = UseRepository.findById(inscripcion.getUsuarioId());
+            if (usuario.getPerfil() == Perfil.ALUMNO){
+                alumno.add(usuario);
+            }
+            else if (usuario.getPerfil() == Perfil.PROFESOR){
+                profesor.add(usuario);
+            }
+            else if (usuario.getPerfil() == Perfil.INVITADO){
+                invitado.add(usuario);
+            }
+
+        }
+
+
+    }
+
+    @Override
+    public Inscripcion inscribirUsuario(Long tallerid, Long usuarioid, Rol rol) {
+        return null;
+    }
+
+    @Override
+    public List<Inscripcion> verResponsables(Long tallerid) {
+        return List.of();
+    }
+
+    @Override
+    public Inscripcion cambiarRol(Long tallerid, Long usuarioid) {
+        return null;
+    }
+
+    @Override
+    public Inscripcion expulsarUsuario(Long tallerid, Long usuarioid) {
+        return null;
+    }
+}
